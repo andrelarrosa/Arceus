67b9046db1d505bddd26b28b571825584949605a
andrelarrosa <andre.larrosa@outlook.com>
2023-03-20 22:38:41 -0300

    Criando Classes do projeto

--- Arceus/cadastros/admin.py
+++ Arceus/cadastros/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Treinador
+from .models import Tipo
+
 
 # Register your models here.
+admin.site.register(Treinador);
+admin.site.register(Tipo);
+admin.site.register(Pokemon);
+admin.site.register(TiposPokemon);
--- Arceus/cadastros/models.py
+++ Arceus/cadastros/models.py
@@ -6,3 +6,22 @@ class Treinador(models.Model):
 
     def __str__(self):
         return f"{self.nome}";
+
+
+class Tipo(models.Model):
+    nome = models.CharField(max_length=50);
+
+    def __str__(self):
+        return f"{self.nome}"
+
+
+class Pokemon(models.Model):
+    nome = models.CharField(max_length=100)
+
+
+class TiposPokemon(models.Model):
+    tipo = models.ForeignKey(Tipo, on_delete=models.PROTECT);
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.PROTECT);
+
+
+
