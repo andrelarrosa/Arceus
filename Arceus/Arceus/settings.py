d61c9a539c0f87d93ac6519fa062a9d8461a8310
aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <aaaaaaaaaacom>
2023-04-17 22:10:56 -0300

    finalizando as configurações de autenticação

--- Arceus/Arceus/settings.py
+++ Arceus/Arceus/settings.py
@@ -139,6 +139,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Configurações de autenticação
 
-LOGIN_URL="login"
+LOGIN_URL = "login"
 LOGOUT_REDIRECT_URL = "login"
-LOGIN_REDIRECT_URL = "login"
+LOGIN_REDIRECT_URL = "listar-treinador"
