73e67ab6d563e3aac10333fc4c69f6bd83747d1d
andrelarrosa <andre.larrosa@outlook.com>
2023-09-18 20:36:37 -0300

    adicionando a tela de cadastro de usuário

--- Arceus/usuarios/urls.py
+++ Arceus/usuarios/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .views import UsuarioCreate
 
 urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("login/", auth_views.LoginView.as_view(
         template_name='cadastros/login.html',
         extra_context={'titulo': 'Autenticação'}), name="login"),
+    path('registrar/', UsuarioCreate.as_view(), name='registrar'),
 ]
--- Arceus/usuarios/views.py
+++ Arceus/usuarios/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from .forms import UsuarioForm 
 
-# Create your views here.
+class UsuarioCreate(CreateView):
+    template_name = "cadastros/form.html"
+    # model = User
+    # fields = ['username', 'email', 'password']
+    form_class = UsuarioForm
+    success_url = '../login'
+
